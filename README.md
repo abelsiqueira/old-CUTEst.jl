9dabf213384be153b51ded8fc636d0f377cb6747
Raniere Silva <raniere@ime.unicamp.br>
2014-02-27 14:58:21 -0300

    Proof of concept of CUTEst interface

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # CUTEst
+
+**WARNING**: This is a work in progress.
+
+Interface to [CUTEst](http://ccpforge.cse.rl.ac.uk/gf/project/cutest/wiki/), a
+Constrained and Unconstrained Testing Environment on steroids, from Dominique
+Orban.
+
+## Installation
+
+- Install CUTEst
+
+  C/Fortran "code to be called (from Julia) must be available as a shared
+  library". Unfortunately the original CUTEst is only available as static
+  library so you have to use [ugly](https://github.com/lpoo/ugly).
+
+~~~
+$ git clone --recursive git://github.com/lpoo/ugly.git
+$ cd ugly
+$ ./configure --enable-shared
+$ make
+# make install
+~~~
+
+- Install this package.
+
+~~~
+julia> Pkg.clone("git://github.com/lpoo/CUTEst.jl.git")
+~~~
+
+## Using
+
+- Starting Julia with current working directory in `LD_LIBRARY_PATH`.
+
+~~~
+LD_LIBRARY_PATH=./ julia
+~~~
+
+- Load CUTEst
+
+
+~~~
+julia> using CUTEst
+~~~
+
+- Build the shared library for wanted problem (this is CUTEst's inheritance).
+
+~~~
+julia> CUTEst.buildCUTEstProb("problem_name")
+~~~
+
+- Load problem build previously.
+
+~~~
+julia> CUTEst.loadCUTEstProb()
+~~~
--- src/CUTEst.jl
+++ src/CUTEst.jl
@@ -2,4 +2,51 @@ module CUTEst
 
 # package code goes here
 
+# CUTEst filename
+fName = "OUTSDIF.d"
+# FORTRAN unit number for OUTSDIF.d
+funit = 42
+# FORTRAN unit number for error output
+iout = 6
+# Exit flag from OPEN and CLOSE
+ioErr = 0
+# FORTRAN unit internal input/output
+io_buffer = 11
+# Exit flag from CUTEst tools
+status = 0
+
+type CUTEstProb
+    # number of variables
+    nvar::Int
+    # number of constraints
+    ncon::Int
+    # nnz in Jacobian
+    nnzj::Int
+    # nnz in upper triangular Hessian
+    nnzh::Int
+    # derivative type
+    dertype::Int
+end
+
+function buildCUTEstProb(prob_name)
+    run(`runcutest -p gen77 -D $prob_name`)
+end
+
+function loadCUTEstProb()
+    ioErr = 0
+    status = 0
+    n = zeros(Int32, 1)
+    m = zeros(Int32, 1)
+    fName = "OUTSDIF.d"
+    funit = 42
+
+    ccall(("fortran_open_",
+           "libCUTEstJL.so"),
+          Ptr{Int32}, (Ptr{Int32}, Ptr{Uint8}, Ptr{Int32}), &funit, fName, &ioErr)
+    ccall(("cutest_cdimen_",
+           "libCUTEstJL.so"),
+          Ptr{Int32}, (Ptr{Int32}, Ptr{Int32}, Ptr{Int32}, Ptr{Int32}), &status, &funit, n, m)
+    return n, m
+end
+
 end # module
