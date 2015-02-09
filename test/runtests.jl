fd309c6994c5a6394499d17ad575516c73a32f0b
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2015-02-09 15:49:38 -0200

    Change where to search test files

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-tests = ["basic_test", "test_unc_julia_functions", "test_cons_julia_functions"]
+tests = ["test_simple", "test_unc_julia_functions", "test_cons_julia_functions"]
 for test in tests
-    run(`julia test/$test.jl`)
+    run(`julia $test.jl`)
 end
