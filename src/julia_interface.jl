c4d635d44437423bf4db1487983100fbf01a60a2
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2015-02-08 22:36:35 -0200

    Implement obj_grad and constr

--- src/julia_interface.jl
+++ src/julia_interface.jl
@@ -11,3 +11,27 @@ function obj_fun(prob, x)
   end
   return f
 end
+
+function obj_grad(prob, x)
+  st = [int32(-1)]
+  f = [0.0]
+  g = Array(Float64, prob.n[1])
+  grad = [int32(1)]
+  if prob.m[1] > 0
+    cofg(st, prob.n, x, f, g, grad)
+  else
+    uofg(st, prob.n, x, f, g, grad)
+  end
+  return g
+end
+
+function constr(prob, x)
+  if prob.m[1] == 0
+    return []
+  end
+  st = [int32(-1)]
+  f = [0.0]
+  c = Array(Float64,prob.m[1])
+  cfn(st, prob.n, prob.m, x, f, c)
+  return c
+end
