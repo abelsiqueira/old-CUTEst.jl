3d94ca8ab04599c9b065f87007ce92a313efa977
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2015-02-09 18:31:40 -0200

    Add objHprod, lagrHprod, objHess and lagrHess

--- src/julia_interface.jl
+++ src/julia_interface.jl
@@ -1,46 +1,114 @@
 include("cutest_functions.jl")
 
 function objFun(prob, x)
-  st = [int32(-1)]
-  f = [0.0]
-  if prob.m[1] > 0
-    c = Array(Float64,prob.m[1])
-    cfn(st, prob.n, prob.m, x, f, c)
-  else
-    ufn(st, prob.n, x, f)
-  end
-  return f
+    st = [int32(-1)]
+    f = [0.0]
+    if prob.m[1] > 0
+        c = Array(Float64,prob.m[1])
+        cfn(st, prob.n, prob.m, x, f, c)
+    else
+        ufn(st, prob.n, x, f)
+    end
+    return f
 end
 
 function objGrad(prob, x)
-  st = [int32(-1)]
-  f = [0.0]
-  g = Array(Float64, prob.n[1])
-  grad = [int32(1)]
-  if prob.m[1] > 0
-    cofg(st, prob.n, x, f, g, grad)
-  else
-    uofg(st, prob.n, x, f, g, grad)
-  end
-  return g
+    st = [int32(-1)]
+    f = [0.0]
+    g = Array(Float64, prob.n[1])
+    grad = [int32(1)]
+    if prob.m[1] > 0
+        cofg(st, prob.n, x, f, g, grad)
+    else
+        uofg(st, prob.n, x, f, g, grad)
+    end
+    return g
+end
+
+function objHprod(prob, x, v)
+    st = [int32(-1)]
+    goth = [int32(0)]
+    Hv = Array(Float64, prob.n[1])
+    if prob.m[1] > 0
+        y = zeros(prob.m[1])
+        chprod(st, prob.n, prob.m, goth, x, y, v, Hv)
+    else
+        uhprod(st, prob.n, goth, x, v, Hv)
+    end
+    return Hv
+end
+
+function lagrHprod(prob, x, y, v)
+    st = [int32(-1)]
+    goth = [int32(0)]
+    Hv = Array(Float64, prob.n[1])
+    if prob.m[1] > 0
+        chprod(st, prob.n, prob.m, goth, x, y, v, Hv)
+    else
+        uhprod(st, prob.n, goth, x, v, Hv)
+    end
+    return Hv
+end
+
+function objHess(prob, x)
+    st = [int32(-1)]
+    nnzh = [int32(0)]
+    Hv = Array(Float64, prob.nnzh[1])
+    Hi = Array(Int32, prob.nnzh[1])
+    Hj = Array(Int32, prob.nnzh[1])
+    if prob.m[1] > 0
+        y = zeros(prob.m[1])
+        csh(st, prob.n, prob.m, x, y, nnzh, prob.nnzh, Hv, Hi, Hj)
+    else
+        ush(st, prob.n, x, nnzh, prob.nnzh, Hv, Hi, Hj)
+    end
+    H = zeros(prob.n[1], prob.n[1])
+    for k = 1:nnzh[1]
+        i = Hi[k]
+        j = Hj[k]
+        H[i,j] = Hv[k]
+        H[j,i] = Hv[k]
+    end
+    return H
+end
+
+function lagrHess(prob, x, y)
+    st = [int32(-1)]
+    nnzh = [int32(0)]
+    Hv = Array(Float64, prob.nnzh[1])
+    Hi = Array(Int32, prob.nnzh[1])
+    Hj = Array(Int32, prob.nnzh[1])
+    if prob.m[1] > 0
+        csh(st, prob.n, prob.m, x, y, nnzh, prob.nnzh, Hv, Hi, Hj)
+    else
+        ush(st, prob.n, x, nnzh, prob.nnzh, Hv, Hi, Hj)
+    end
+    H = zeros(prob.n[1], prob.n[1])
+    for k = 1:nnzh[1]
+        i = Hi[k]
+        j = Hj[k]
+        H[i,j] = Hv[k]
+        H[j,i] = Hv[k]
+    end
+    return H
 end
 
 function constr(prob, x)
-  if prob.m[1] == 0
-    return []
-  end
-  st = [int32(-1)]
-  f = [0.0]
-  c = Array(Float64,prob.m[1])
-  cfn(st, prob.n, prob.m, x, f, c)
-  return c
+    if prob.m[1] == 0
+        return []
+    end
+    st = [int32(-1)]
+    f = [0.0]
+    c = Array(Float64,prob.m[1])
+    cfn(st, prob.n, prob.m, x, f, c)
+    return c
 end
 
 function terminate(prob)
-  st = [int32(-1)]
-  if prob.m[1] > 0
-    cterminate(st)
-  else
-    uterminate(st)
-  end
+    st = [int32(-1)]
+    if prob.m[1] > 0
+        cterminate(st)
+    else
+        uterminate(st)
+    end
 end
--- test/test_unc_julia_functions.jl
+++ test/test_unc_julia_functions.jl
@@ -7,8 +7,15 @@ x = prob.x
 f = CUTEst.objFun(prob, x)
 g = CUTEst.objGrad(prob, x)
 c = CUTEst.constr(prob, x)
+H = CUTEst.objHess(prob, x)
+W = CUTEst.lagrHess(prob, x, c)
+Hg = CUTEst.objHprod(prob, x, g)
+Wg = CUTEst.lagrHprod(prob, x, c, g)
+
 println("f = ", f)
 println("g = ", g)
 println("c = ", c)
+println("|Hg - H*g| = ", norm(Hg-H*g))
+println("|Wg - W*g| = ", norm(Wg-W*g))
 
 CUTEst.terminate(prob)
